2f12fe83e160a5b71732a6de52cfb5400e21a549
ashrafulislampro <ashrafulislam1659@gmail.com>
2020-08-08 14:31:38 +0600

    Done

---
+++ true-false.js
@@ -0,0 +1,18 @@
+// const age = 0;
+//falsy
+//undefined
+//0 
+//""
+//null
+//NaN
+//truthy
+//" " || "0"
+
+
+let name = NaN;
+console.log(name);
+if(name ){
+    console.log("Condition is true");
+}else{
+    console.log("Condition is false");
+}
