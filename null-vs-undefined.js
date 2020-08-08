4b3474a82166ab2cd65de2c82ceb54b5f9ffb2af
ashrafulislampro <ashrafulislam1659@gmail.com>
2020-08-08 15:02:00 +0600

    null-vs-undefined cleared

---
+++ null-vs-undefined.js
@@ -0,0 +1,32 @@
+let pakhi;
+console.log(pakhi);
+
+function add(num1, num2){
+    console.log(num1 + num2);
+}
+const result = add(34, 45);
+console.log(result);
+
+function add(num1, num2){
+    console.log(num1 + num2);
+    return  
+}
+const result = add(34, 45);
+console.log(result);
+
+function add(num1, num2){
+    console.log(num1, num2);
+}
+const result = add(34 );
+console.log(result);
+
+const premik = {name: "Akash Miah", Phone: 013456566}
+console.log(premik.gf);
+
+let  fun = undefined;
+console.log(fun);
+
+let ages = [12, 34, 45, 56];
+console.log(ages[22]);
+
+
